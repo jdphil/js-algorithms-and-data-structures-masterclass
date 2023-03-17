f11f87c04523d7f5c6963e858a0d9c277d097061
Jon Phillipo <jdphil@gmail.com>
2023-03-17 14:44:09 -0400

    linked list

---
+++ 110.linkedlist.js
@@ -0,0 +1,43 @@
+//What is a linked list?
+/*
+no indices but nodes, and each node has a value and pointer 
+to another node or null (like blockchain)
+
+like a skyscraper without an elevator 
+have to take the stairs and go floor by floor 
+
+singly is because it only links to the next node
+*/
+
+//piece of data 
+// reference to next node - next
+class Node {
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head
+        }
+        else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+            this.length++;     
+        }
+        return this;
+    }
+    pop(){
+        if(this.length===0) return undefined;
+    }
+}
