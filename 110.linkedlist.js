c88a9d9f92d64178dfcfe61d472ebfd6ba1f447a
Jon Phillipo <jdphil@gmail.com>
2023-08-08 15:01:19 -0500

    unshift

--- 110.linkedlist.js
+++ 110.linkedlist.js
@@ -12,48 +12,69 @@ singly is because it only links to the next node
 //piece of data 
 // reference to next node - next
 class Node {
-    constructor(val){
+    constructor(val) {
         this.val = val;
         this.next = null;
     }
 }
 
-class SinglyLinkedList{
-    constructor(){
+class SinglyLinkedList {
+    constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
-    push(val){
+    push(val) {
         let newNode = new Node(val);
-        if(!this.head){
+        if (!this.head) {
             this.head = newNode;
             this.tail = this.head
         }
-        else{
+        else {
             this.tail.next = newNode;
             this.tail = newNode;
         }
-        this.length++;     
+        this.length++;
         return this;
     }
-    pop(){
-        if(this.length===0) return undefined;
+    pop() {
+        if (this.length === 0) return undefined;
         var current = this.head;
         var newTail = current;
-        while(current.next){
+        while (current.next) {
             newTail = current;
             current = current.next;
         }
         this.tail = newTail;
         this.tail.next = null;
         this.length--;
-        if(this.length===0){
+        if (this.length === 0) {
             this.head = null;
             this.tail = null;
         }
         return current;
     }
+    shift() {
+        if (!this.head) return undefined;
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        return currentHead;
+    }
+    unshift(val) {
+        var newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
 }
 
 var list = new SinglyLinkedList();
