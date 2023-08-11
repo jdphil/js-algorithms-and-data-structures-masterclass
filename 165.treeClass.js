a401a59bb01cb68e9c8ea6d04e294a55d35368bd
Jon Phillipo <jdphil@gmail.com>
2023-08-11 09:55:28 -0500

    working

--- 165.treeClass.js
+++ 165.treeClass.js
@@ -1,31 +1,46 @@
 class Node {
-    constructor(value){
+    constructor(value) {
         this.value = value;
-        tthis.left = null;
+        this.left = null;
         this.right = null;
     }
 }
 
 class BinarySearchTree {
-    constructor(){
+    constructor() {
         this.root = null;
     }
-    insert(value){
+    insert(value) {
         var newNode = new Node(value);
-        if(!this.root){
+
+        if (!this.root) {
             this.root = newNode;
+            return this;
         }
-        else{
-            var currentNode = this.root;
-            while(currentNode){
-                if(value < currentNode.value){
-                    currentNode = this.root.left;
+
+        var currentNode = this.root;
+        while (true){
+            if(value === currentNode.value) return undefined;
+            
+            if (value > currentNode.value) {
+                if (currentNode.right) {
+                    currentNode = currentNode.right;
                 }
-                else{
-                    currentNode = this.root.right;
+                else {
+                    currentNode.right = newNode;
+                    return this;
                 }
             }
-
+    
+            if (value < currentNode.value) {
+                if (currentNode.left) {
+                    currentNode = currentNode.left;
+                }
+                else {
+                    currentNode.left = newNode;
+                    return this;
+                }
+            }
         }
 
     }
@@ -33,7 +48,11 @@ class BinarySearchTree {
 
 
 var tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left = new Node(9);
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(16)
+tree.insert(7)
+tree.insert(8)
