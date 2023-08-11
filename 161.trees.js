e8879ff8549bc94e27a1caeb033d464ea0cfb519
Jon Phillipo <jdphil@gmail.com>
2023-08-11 08:23:01 -0500

    trees intro

---
+++ 161.trees.js
@@ -0,0 +1,25 @@
+//whats a tree? nodes (like a linked list) parent / child relationship / branching 
+//upside down tree... top down
+//                     2
+//          9  12 8 99                 10
+//      2     1 7  2   44              11  55 87
+
+
+//list are linear
+//trees are not - linked lists have one path
+//trees have many paths
+// SLL - is a special tree with only one pathway 
+
+//a node can only point to a child / that would be a graph 
+
+//a tree must have a root one entry point top point in a tree
+//a child is a node directly connected to another node 
+//parent the converse notion of a child 
+//siblings - a group of nodes with the same parent 
+//leaf - a node with no children 
+//edge - the connection between one node and another
+
+//kinds of trees 
+//tress
+//binary trees
+//binary search trees 
