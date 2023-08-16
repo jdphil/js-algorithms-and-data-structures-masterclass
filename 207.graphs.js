12af2412c361f45b5c76fc0a40ffac5df7732b74
Jon Phillipo <jdphil@gmail.com>
2023-08-16 12:04:18 -0500

    my attempt - loops and if statements not working

--- 207.graphs.js
+++ 207.graphs.js
@@ -9,7 +9,58 @@ Different types of graphs - compare
         Undirected - No direction on an edge - facebook 
         Driected Graph - Represented with arrows - instagram - follow one way 
         Weighted Graph - assign a value to the edges (mev arb?)
-Implement a graph using an adjacency list 
+Implement a graph using an adjacency list vs matrix
+    List can take up less space (in  sparse graphs)
+    faster to iterate over all edges
+    can be slower to lookup specific edge
+    maxtrix takes up more space in sparse graphs 
+    slower to iterate over all edges 
+    faster to lookup specific edge
+    nested array / hash table
+    index for a given key - detail which nodes it is connected to (the edges)
+    real world data tends to be sparse 
 Traverse through the graph - BFS and DFS
 Compare traversal
 */
+
+class Graph {
+    constructor(){
+        this.adjacencyList = {};
+    }
+    addVertex(vertex){
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];// need to handle dupes 
+    }
+    addEdge(v1, v2){
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
+    removeEdge(v1, v2){
+        //v1
+        let temp1 = this.adjacencyList[v1];
+        for(let i=0;i<temp1.length;i++){
+            if(temp1[i]===v2){
+                temp1.pop();
+            }
+        }
+        this.adjacencyList[v2] = temp1;
+
+        //v2
+        let temp2 = this.adjacencyList[v2];
+        for(let i=0;i<temp1.length;i++){
+            if(temp2[i]===v1){
+                temp2.pop();
+            }
+        }
+        this.adjacencyList[v2] = temp2;
+    }
+}
+
+let g = new Graph();
+g.addVertex("Aspen")
+g.addVertex("Tokyo")
+g.addVertex("Dallas")
+g.addEdge("Dallas","Tokyo")
+g.addEdge("Aspen","Dallas")
+console.log(g)
+g.removeEdge("Dallas","Tokyo")
+console.log(g)
