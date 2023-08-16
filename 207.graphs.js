de323b27167bb3db46be2f23dcf7a48d4ba4c9d6
Jon Phillipo <jdphil@gmail.com>
2023-08-16 10:50:57 -0500

    starting graphs

---
+++ 207.graphs.js
@@ -0,0 +1,7 @@
+/*
+Graphs - what is it? Nodes + Connections to those nodes 
+Different types of graphs - compare
+Implement a graph using an adjacency list 
+Traverse through the graph - BFS and DFS
+Compare traversal
+*/
