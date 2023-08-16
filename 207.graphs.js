05ec711bfa80f26a3d2a41a862f0b02582dd386c
Jon Phillipo <jdphil@gmail.com>
2023-08-16 11:23:33 -0500

    notes

--- 207.graphs.js
+++ 207.graphs.js
@@ -1,6 +1,14 @@
 /*
 Graphs - what is it? Nodes + Connections to those nodes 
+    uses for graphs - social netowrks, location / mapping, routing (mev arb)
+Essential Graph Terms : 
+    Vertex - Node
+    Edge - Connection between nodes
 Different types of graphs - compare
+    Undirected vs Direct Graphs
+        Undirected - No direction on an edge - facebook 
+        Driected Graph - Represented with arrows - instagram - follow one way 
+        Weighted Graph - assign a value to the edges (mev arb?)
 Implement a graph using an adjacency list 
 Traverse through the graph - BFS and DFS
 Compare traversal
