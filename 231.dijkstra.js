623fd09b2ea1a37fd63f9143c59c39f367766d1d
Jon Phillipo <jdphil@gmail.com>
2023-08-19 10:33:12 -0500

    dijkstra working with priority queue

--- 231.dijkstra.js
+++ 231.dijkstra.js
@@ -30,7 +30,7 @@ class WeightedGraph {
 
         //as long as there is something to visit
         while(nodes.values.length){
-            smallest = nodes.dequeue().val;
+            smallest = nodes.dequeue().value;
             if(smallest === finish){
                 console.log(distances);
                 console.log(previous);
@@ -66,21 +66,114 @@ class WeightedGraph {
 }
 
 
+/*Heaps 
+heap is a type of tree... we are focused on binary heap 
+
+what is a binary heap? very similar to bst, but different rules
+    max binary heap - parent nodes are always larger than child
+    min binary heap - parent nodes are always smaller than child 
+    parent can only have zero one or two nodes, can not have three hence binary 
+
+    no guaranteeds between the siblings 
+compare and contrast min and max heap 
+implement basic methods
+where are they used in real world? priority queue
+*/
+
+/*Representing a heap - can use an array
+For any index of an array n... 
+The left child is stored at 2n+1
+The right child is stored at 2n+2
+
+e.g. parent n=5
+left child = 2n+1 = 2*5+1 = 11
+right child = 2n+2 = 2*5+2 = 12
+
+can also find parent from child 
+n-1/2 floored
+*/
+
+class Node{
+    constructor(value, priority){
+        this.value = value;
+        this.priority = priority;
+    }
+}
+
 class PriorityQueue {
     constructor() {
         this.values = [];
     }
-    enqueue(val, priority) {
-        this.values.push({ val, priority });
-        this.sort();
+    enqueue(value, priority) {
+        let newNode = new Node(value, priority);
+        function findParent(index) {
+            return Math.floor((index - 1) / 2);
+        }
+
+        this.values.push(newNode);
+        var index = this.values.length - 1;
+        var parentIndex = findParent(index);
+        const element = this.values[index];
+        while (index > 0) {
+            var parentIndex = findParent(index);
+            //swap
+            let parent = this.values[parentIndex];
+            if (element.priority >= parent.priority) break;
+            this.values[parentIndex] = element;
+            this.values[index] = parent
+            //update i 
+            index = parentIndex;
+        }
+        return this.values;
+    }
+    sinkDown() {
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+
+        while (true) {
+            let leftChildIdx = ((2 * idx) + 1);
+            let rightChildIdx = ((2 * idx) + 2);
+            let leftChild, rightChild;
+            let swap = null;
+
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx];
+                if(leftChild.priority < element.priority){
+                    swap = leftChildIdx;
+                }
+            }
+
+            if(rightChildIdx < length){
+                rightChild = this.values[rightChildIdx];
+                if(
+                    (swap === null && rightChild.priority < element.priority) ||
+                    (swap !== null && rightChild.priority < leftChild.priority)
+                 ) {
+                    swap = rightChildIdx;
+                }
+            }
+
+            if(swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
     }
     dequeue() {
-        return this.values.shift();
+        if(this.values.length === 1) return this.values.pop();
+        var min = this.values[0];
+        var end = this.values.pop();
+        this.values[0] = end;
+        this.sinkDown();
+        return min;
+
+
+
     }
-    sort() {
-        this.values.sort((a, b) => a.priority - b.priority);
-    };
 }
+
+//no guaranteed order between siblings
 
 var g = new WeightedGraph();
 
@@ -103,4 +196,4 @@ g.addEdge("E", "F", 1);
 
 console.log(g.adjacencyList);
 
-g.dijkstra("A", "F");
+g.dijkstra("A", "E");
